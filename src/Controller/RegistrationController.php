91dcc1ee40ea5b53ffd8b4fedd5727f7dd96ee1b
Alan Guedes <alanguedes00@live.com>
2022-09-27 23:02:25 -0300

    Adicionando Media Bundle

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -2,13 +2,13 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Entity\Doc\User;
 use Doctrine\Persistence\ManagerRegistry;
-use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/api", name="api_")
--- src/Entity/Doc/User.php
+++ src/Entity/Doc/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Entity;
+namespace App\Entity\Doc;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
